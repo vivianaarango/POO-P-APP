1842c85c3cef7fb335887612e01f3fb27facf90c
Viviana Arango <viviana.arango@logisticapp.org>
2019-06-07 15:36:41 -0500

    web view

--- app/src/main/java/com/project/poop/Fragments/ListFragment.java
+++ app/src/main/java/com/project/poop/Fragments/ListFragment.java
@@ -1,27 +1,29 @@
 package com.project.poop.Fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.project.poop.R;
-import com.project.poop.activities.CorrectActivity;
-import com.project.poop.activities.FailedActivity;
 import com.project.poop.libraries.Base;
 import com.project.poop.libraries.CheckConexion;
 import com.project.poop.libraries.InterfaceRetrofit;
 import com.project.poop.managers.ManageSharedPreferences;
 import com.project.poop.managers.ManagerProgressDialog;
-import com.project.poop.models.question.ResponseAnswer;
-import com.project.poop.models.themes.DataTheme;
 import com.project.poop.models.themes.ResponseTheme;
 
 import java.util.ArrayList;
@@ -50,7 +52,8 @@ public class ListFragment extends Fragment {
     private ManagerProgressDialog progress;
     private ManageSharedPreferences manageSharedPreferences;
     private CheckConexion checkConexion;
-    private ArrayList<DataTheme> dataTheme;
+    private ImageButton search;
+
 
     public ListFragment() {
         // Required empty public constructor
@@ -66,6 +69,14 @@ public class ListFragment extends Fragment {
 
         listView = (ExpandableListView) view.findViewById(R.id.listView);
         initData();
+
+        search = view.findViewById(R.id.btn_search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                search();
+            }
+        });
 
         return view;
     }
@@ -94,37 +105,6 @@ public class ListFragment extends Fragment {
 
         getThemes();
 
-       /* listDataHeader.add("EDMTDev");
-        listDataHeader.add("Android");
-        listDataHeader.add("Xamarin");
-        listDataHeader.add("UWP");
-
-        List<String> edmtDev = new ArrayList<>();
-        edmtDev.add("This is Expandable ListView");
-
-        List<String> androidStudio = new ArrayList<>();
-        androidStudio.add("Expandable ListView");
-        androidStudio.add("Google Map");
-        androidStudio.add("Chat Application");
-        androidStudio.add("Firebase ");
-
-        List<String> xamarin = new ArrayList<>();
-        xamarin.add("Xamarin Expandable ListView");
-        xamarin.add("Xamarin Google Map");
-        xamarin.add("Xamarin Chat Application");
-        xamarin.add("Xamarin Firebase ");
-
-        List<String> uwp = new ArrayList<>();
-        uwp.add("UWP Expandable ListView");
-        uwp.add("UWP Google Map");
-        uwp.add("UWP Chat Application");
-        uwp.add("UWP Firebase ");
-
-        listHash.put(listDataHeader.get(0),edmtDev);
-        listHash.put(listDataHeader.get(1),androidStudio);
-        listHash.put(listDataHeader.get(2),xamarin);
-        listHash.put(listDataHeader.get(3),uwp);
-        */
     }
 
     private void getThemes() {
@@ -173,4 +153,19 @@ public class ListFragment extends Fragment {
 
         }
     };
+
+
+    private void search() {
+        /*ListFragment fragment2 = new ListFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_webview, fragment2);
+        fragmentTransaction.commit();*/
+        WebViewFragment nextFrag= new WebViewFragment();
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_list, nextFrag, "findThisFragment")
+                .addToBackStack(null)
+                .commit();
+    }
+
 }
---
+++ app/src/main/java/com/project/poop/Fragments/WebViewFragment.java
@@ -0,0 +1,41 @@
+package com.project.poop.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.project.poop.R;
+
+public class WebViewFragment extends Fragment {
+
+    private Context thiscontext;
+    private WebView webView;
+
+    public WebViewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_webview, container, false);
+
+        thiscontext = getActivity();
+
+        webView = (WebView) view.findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        return view;
+    }
+}
